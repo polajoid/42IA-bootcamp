c9b555da12591ee928a075f91d4a708de261bbdb
jbosslady <jblepany@outlook.fr>
2025-11-19 18:35:12 +0100

    handles non numeric argument with assertion error

--- m00/ex02/whois.py
+++ m00/ex02/whois.py
@@ -2,15 +2,15 @@ from sys import argv
 
 
 def convert():
-    assert len(argv) != 1
-    assert int(argv[1])
+    assert len(argv) == 2, "more than one argument is provided"
+    assert argv[1].isnumeric(), "argument is not an integer"
     num = int(argv[1])
     if (num == 0):
         print("I'm Zero.")
-    elif (num.iseven()):
-        print("I'm Even.")
-    else:
+    elif (num & 1):
         print("I'm Odd.")
+    else:
+        print("I'm Even.")
 
 
 convert()
