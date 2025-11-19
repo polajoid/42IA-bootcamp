ee67612fff0991f1d61c27d75f95d171d668995d
jbosslady <jblepany@outlook.fr>
2025-11-19 16:26:49 +0100

    trying now to reverse thestr

---
+++ m00/ex01/exec.py
@@ -0,0 +1,14 @@
+# Simple exercise : a program that reverse a string given a parameter
+
+import sys
+
+def reverse(*args):
+    result = ""
+    for i in range(1, len(sys.argv)):
+        result += str(sys.argv[i])
+        if (i != len(sys.argv) - 1):
+            result +=  " "
+    result.swapcase()
+    print(result)
+
+reverse(sys.argv)
