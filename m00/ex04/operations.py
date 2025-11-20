e2421282521622f5837aaedc865207e9a6797810
jbosslady <jblepany@outlook.fr>
2025-11-20 23:35:22 +0100

    almost good for the operation part. need to fix the args number handling now

--- m00/ex04/operations.py
+++ m00/ex04/operations.py
@@ -1,18 +1,22 @@
 from sys import argv
 
+
 def operation(param):
     assert param[1].isnumeric() and param[2].isnumeric(), "only integers"
 
     a = int(param[1])
     b = int(param[2])
 
-    print(f"Sum:\t {a + b}")
+    print(f"Sum:\t\t {a + b}")
     print(f"Difference:\t {a - b}")
     print(f"Product:\t {a * b}")
-    print(f"Quotient:\t {a / b}")
-    print(f"Remainder:\t {a % b}")
+    try:
+        print(f"Quotient:\t {a / b}")
+        print(f"Remainder:\t {a % b}")
+    except ZeroDivisionError:
+        print("ERROR (division by zero)")
+        print("ERROR (remainer by zero)")
 
 
 if (len(argv) == 3):
-    # print(f"Len {len(argv)}, argv[0] {argv[2]}")
-    operation(str(argv))
+    operation(argv)
