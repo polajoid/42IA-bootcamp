2c9b8cb8b0bbe0300e3c93e0228cc291aabfd1f5
Jean-Baptiste Lepany <jlepany@f2r5s18.clusters.42paris.fr>
2025-11-20 13:49:18 +0100

    function handles most wrong input and error cases.

--- m00/ex03/count.py
+++ m00/ex03/count.py
@@ -1,7 +1,10 @@
-def text_analyzer(string):
-    assert isinstance(string, str), " argument is not a string"
-    if (string == ""):
-        string = input("What is the text to analyze?")
+def text_analyzer(*arg):
+    assert len(arg) <= 1, "too many arguments"
+    if (len(arg) == 1):
+        assert isinstance(arg[0], str), " argument is not a string"
+        string = arg[0]
+    elif (len(arg) == 0):
+        string = input("What is the text to analyze? ")
     print(f"The text contains {len(string)} printable characters(s):")
     print(f"- {sum(1 for c in string if c.isupper())} upper letter(s)")
     print(f"- {sum(1 for c in string if c.islower())} lower letter(s)")
