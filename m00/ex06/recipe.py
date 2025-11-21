a27c7705aa9fc299714d0201ac2ea3da12a414bb
Jean-Baptiste Lepany <jlepany@f1br3s4.clusters.42paris.fr>
2025-11-21 15:49:12 +0100

    part1 is good

--- m00/ex06/recipe.py
+++ m00/ex06/recipe.py
@@ -1,1 +1,17 @@
-cookbook = {}
+cookbook = {
+    "Sandwich" : {
+        "ingredients" : ("ham", "bread", "cheese", "tomatoes"),
+        "meal" : "lunch",
+        "prep_time" : 10
+    },
+    "Cake" : {
+        "ingredients" : ("flour", "sugar", "eggs"),
+        "meal" : "dessert",
+        "prep_time" : 60
+    },
+    "Salad" : {
+        "ingredients" : ("avocado", "arugula", "tomatoes", "spinach"),
+        "meal" : "lunch",
+        "prep_time" : 60
+    }
+}
