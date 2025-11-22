47f989e95314f779853453752b4d3d512421d451
jbosslady <jblepany@outlook.fr>
2025-11-22 02:29:02 +0100

    function ft_progress show with yield and with tqdm advancement over a for loop

---
+++ m00/ex10/loading.py
@@ -0,0 +1,16 @@
+from tqdm import tqdm
+from time import sleep
+
+
+def ft_progress(lst):
+    for i in tqdm(lst, desc="ETA: ", ascii=" >="):
+        yield i
+
+
+listy = range(3333)
+ret = 0
+for elem in ft_progress(listy):
+    ret += elem
+    sleep(0.005)
+print()
+print(ret)
